29133979e7c9965bf72b6c11f344aca76ee65366
kolopez@puce.edu.ec <kolopez@puce.edu.ec>
2024-07-13 12:25:57 -0500

    update 1

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+@admin.register(Movie)
+class MoviesAdmin(admin.ModelAdmin):
+    pass
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField(null=False, primary_key=True)
+    title = models.CharField(max_length=100, null=False)
+    genre = models.CharField(null=False)
+    director_name = models.CharField(null=False)
+    release_year = models.IntegerField(null=False)
+    sinopsis = models.TextField(null=False)
+
+    def __str__(self) -> str:
+        return self.name
